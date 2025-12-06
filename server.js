447ec587669ed5f6cef170800860a13e82247066
Mai-Thanh-Thuan-15-4-2 <20130127@st.hcmuaf.edu.vn>
2025-12-07 00:40:53 +0700

    Update CORS to allow all origins

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://game-socket-7fls.vercel.app"],
+    origin: ["http://localhost:3000", "https://game-socket-7fls.vercel.app", "*"],
     methods: ["GET", "POST"],
     credentials: true
   },
